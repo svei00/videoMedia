f326ff4efc73f6381f6c705a96db3a5d31d36640
svei00 <79055634+svei00@users.noreply.github.com>
2024-08-02 22:46:14 -0700

    Still working on card component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -24,7 +24,7 @@ interface Movie {
 
 const Results = ({results}: ResultsProps): JSX.Element => {
   return (
-    <div>
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
       {results.map((result) => (
     <div key={result.id}>
         <Card key={result.id} result={result} />
