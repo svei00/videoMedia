a989a8238010bc510d9569638d1fe8f3df549e9b
svei00 <79055634+svei00@users.noreply.github.com>
2024-08-06 18:12:34 -0700

    Deploying the app second intent

--- src/app/search/[searchTerm]/page.tsx
+++ src/app/search/[searchTerm]/page.tsx
@@ -15,6 +15,10 @@ interface MovieResult {
     release_date: string;
     vote_average: number;
     vote_count: number;
+    popularity: number;
+    original_language: string;
+    original_title: string;
+    genre_ids: number[];
 }
 
 interface SearchResponse {
