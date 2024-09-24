0f598c373a7fc9fce582f72e4bfc9a2e34c63304
svei00 <79055634+svei00@users.noreply.github.com>
2024-09-23 23:22:43 -0700

    Debigging result.tdx file

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -16,20 +16,23 @@ interface Movie {
     genre_ids: number[];
     video: boolean;
     adult: boolean;
-  }
+    media_type?: 'movie' | 'tv';
+    name?: string;
+    first_air_date?: string;
+}
 
-  interface ResultsProps {
+interface ResultsProps {
     results: Movie[];
-  }
+}
 
 const Results = ({results}: ResultsProps): JSX.Element => {
   return (
     <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
       {results.map((result) => (
-    <div key={result.id}>
-        <Card key={result.id} result={result} />
-    </div>
-))}
+        <div key={result.id}>
+            <Card key={result.id} result={result} />
+        </div>
+      ))}
     </div>
   )
 }
