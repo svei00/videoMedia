81cf819486954dbd4a99385b9c98f149f7680504
svei00 <79055634+svei00@users.noreply.github.com>
2024-09-24 23:54:35 -0700

    Recovering the information before the watchlist

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -26,11 +26,6 @@ interface ResultsProps {
 }
 
 const Results = ({results}: ResultsProps): JSX.Element => {
-  const [watchlist, setWatchlist] = useState([]); // Array to store movie IDs
-
-  const addToWatchlist = (movieId) => {
-    setWatchlist([...watchlist, movieId]); // Add movie ID to the watchlist state
-  };
   return (
     <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
       {results.map((result) => (
