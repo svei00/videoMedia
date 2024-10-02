18e44846832acec934a42b5471b610d0482c8cd0
svei00 <79055634+svei00@users.noreply.github.com>
2024-10-01 23:55:54 -0700

    added more radious in the bottom of the under construction page

--- src/components/UnderConstruction.tsx
+++ src/components/UnderConstruction.tsx
@@ -6,7 +6,7 @@ interface UnderConstructionProps {
 
 const UnderConstruction: React.FC<UnderConstructionProps> = ({ pageName }) => {
   return (
-    <div className="p-4 mb-20 bg-yellow-100 border border-yellow-400 text-yellow-700 rounded">
+    <div className="p-4 mb-20 bg-yellow-100 border border-yellow-400 text-yellow-700 rounded-xl">
       <h2 className="text-xl font-bold mb-2">Under Construction</h2>
       <p className="mb-2">The {pageName} page is under construction.</p>
       <p className="mb-4">Please check back later!</p>
